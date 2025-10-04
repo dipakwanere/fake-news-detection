56ae08859a9415a5a1236934811c86380efd1a67
Dipak_Wanere <dipakwanere2000@gmail.com>
2025-10-05 00:32:51 +0530

    model train and predict

--- app/main.py
+++ app/main.py
@@ -9,6 +9,12 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from src.predict import predict_texts
+from src.train import train_model  # <-- import your training function
+
+MODEL_PATH = "models/best_model.pkl"
+if not os.path.exists(MODEL_PATH):
+    print("Model not found. Training model now...")
+    train_model()
 
 MODELS_LOADED = False
 MODEL_METRICS = {}
